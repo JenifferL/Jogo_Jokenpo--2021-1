c343dc42f001ab983f409504d446835dbe5b0a08
Jeni <70913017+JenifferL@users.noreply.github.com>
2021-05-01 15:49:01 -0300

    Update Oponente.java

--- Oponente.java
+++ Oponente.java
@@ -4,46 +4,16 @@ import java.util.Random;
 
 public class Oponente {
 
-	private int nome;
-	private String nomeConvertido;
 	private int sistema;
 	private String sistemaConvertido;
 
-	public void nomeOponente() {
-		Random nome = new Random();
-
-		for (int i = 0; i < 3; i++) {
-			this.setNome(nome.nextInt(4));
-		}
-		
-		switch (getNome()) {	
-			case 0:
-				setNomeConvertido("Evandro");
-				break;
-				
-			case 1:
-				setNomeConvertido("Jeni");
-				break;
-				
-			case 2:
-				setNomeConvertido("Roberta");
-				break;
-				
-			case 3:
-				setNomeConvertido("Thiago");
-				break;
-		}
-		
-		System.out.println("Nome do seu oponente: " + getNomeConvertido());
-	}
-
 	public void escolhaOponente() {
 		Random cpu = new Random();
-		
+
 		for (int i = 0; i < 2; i++) {
 			this.setSistema(cpu.nextInt(3));
 		}
-		
+
 		if (getSistema() == 0) {
 			setSistemaConvertido("Pedra");
 		} else if (getSistema() == 1) {
@@ -51,10 +21,10 @@ public class Oponente {
 		} else {
 			setSistemaConvertido("Tesoura");
 		}
-		
-		System.out.println("Escolha do seu oponente: " + getSistemaConvertido());
-		
-		getSistemaConvertido();
+
+		System.out.println("O Computador escolheu: " + getSistemaConvertido());
+		System.out.println();
+
 	}
 
 	public int getSistema() {
@@ -71,22 +41,6 @@ public class Oponente {
 
 	public void setSistemaConvertido(String conversao) {
 		this.sistemaConvertido = conversao;
-	}
-
-	public int getNome() {
-		return this.nome;
-	}
-
-	public void setNome(int nome) {
-		this.nome = nome;
-	}
-
-	public String getNomeConvertido() {
-		return this.nomeConvertido;
-	}
-
-	public void setNomeConvertido(String nomeConvertido) {
-		this.nomeConvertido = nomeConvertido;
 	}
 
 }
