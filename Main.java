3969823c4efd6b89560abdfde63d578c04dbc2e4
Jeni <70913017+JenifferL@users.noreply.github.com>
2021-05-01 15:48:38 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Usuario jogador1 = new Usuario();
-		Oponente oponente = new Oponente();
+		Oponente nome = new Oponente();
 		Scanner scanner = new Scanner(System.in);
 		Resultado resultado = new Resultado();
-		
+
 		System.out.println("Digite seu nome: ");
-		jogador1.setNome(scanner.nextLine());		
-		
-		System.out.println("Digite sua opcao (Pedra, papel ou tesoura): ");						   
+		jogador1.setNome(scanner.nextLine());
+		System.out.println("Escolha uma das opçõe a seguir: PEDRA, PAPEL ou TESOURA.");
+
 		jogador1.setEscolha(scanner.nextLine());
-		
-		oponente.nomeOponente();
-		oponente.escolhaOponente();
-		
-		resultado.geraResultado(jogador1, oponente);
+
+		nome.escolhaOponente();
+
+		resultado.geraResultado(jogador1, nome);
 	}
 
 }
